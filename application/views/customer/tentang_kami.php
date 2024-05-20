e4edc766732fd94c1c285df0b440d53dc1d4ecf5
XionN <maditya919.ma@gmail.com>
2024-05-20 12:02:58 +0700

    merubah heading ke paragraf

--- application/views/customer/tentang_kami.php
+++ application/views/customer/tentang_kami.php
@@ -23,16 +23,16 @@
 				<div class="contact-form card card-body">
 					<h5 class="text-center mb-4">Selamat datang di website kami.</h5>
 
-					<h5 class="mb-4">Kami adalah jasa penyedia transportasi di Depok, dengan didukung staff dan supir berpengalaman dengan berbagai jenis armada yang nyaman yang dapat dipergunakan untuk keperluan dinas, wisata, maupun untuk acara-acara yang resmi. Dalam penyediaan kendaraan tersebut kami selalu berusaha untuk mengutamakan pelayanan kepada para konsumen kami, dimana kepuasan konsumen merupakan tujuan kami.</h5>
+					<p class="mb-4">Kami adalah jasa penyedia transportasi di Depok, dengan didukung staff dan supir berpengalaman dengan berbagai jenis armada yang nyaman yang dapat dipergunakan untuk keperluan dinas, wisata, maupun untuk acara-acara yang resmi. Dalam penyediaan kendaraan tersebut kami selalu berusaha untuk mengutamakan pelayanan kepada para konsumen kami, dimana kepuasan konsumen merupakan tujuan kami.</p>
 
-					<h5><i class="fa fa-map-marker"></i> Alamat:</h5>
+					<p><i class="fa fa-map-marker"></i> Alamat:</p>
 
-					<h5 class="mb-4"><font color="#02b7ff">Jl. Margonda No.8, Pondok Cina, Kecamatan Beji, Kota Depok, Jawa Barat 16424</font></h5>
+					<p class="mb-4"><font color="#070c85">Jl. Margonda No.8, Pondok Cina, Kecamatan Beji, Kota Depok, Jawa Barat 16424</font></p>
 
-					<h5 class="mb-4"><i class="fa fa-whatsapp"></i> No. WA: <font color="#02b7ff">(0895) 322882891</font></h5>
-					<h5 class="mb-4"><i class="fa fa-comment"></i> No. Telp/SMS: <font color="#02b7ff">(0895) 322882891</font></h5>
-					<h5 class="mb-5"><i class="fa fa-envelope"></i> E-mail: <font color="#02b7ff">baskororentcar@gmail.com</font></h5>
-					<h5 class="text-right">Ttd, Ramadhan Arifiyanto (1841720078).</h5>
+					<p class="mb-4"><i class="fa fa-whatsapp"></i> No. WA: <font color="#070c85">(0895) 322882891</font></p>
+					<p class="mb-4"><i class="fa fa-comment"></i> No. Telp/SMS: <font color="#070c85">(0895) 322882891</font></p>
+					<p class="mb-5"><i class="fa fa-envelope"></i> E-mail: <font color="#070c85">baskororentcar@gmail.com</font></p>
+					<p class="text-right">Ttd, Ramadhan Arifiyanto (1841720078).</p>
 				</div>
 			</div>
 		</div>
